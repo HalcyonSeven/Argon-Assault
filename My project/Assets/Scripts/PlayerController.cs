1ee9a16d51cc486e91f70e544b6d07b35c04051b
Tristan Devadason <67545352+HalcyonSeven@users.noreply.github.com>
2023-05-08 23:35:05 +0800

    clean up, headers

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("General Setup Settings")]
     [SerializeField] float controlSpeed = 10f;
-    [SerializeField] float xRange = 10f;
+    [SerializeField] float xRange = 14f;
     [SerializeField] float positionPitchFactor = -2f;
     [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float positionYawFactor = 2f;
     [SerializeField] float controlRollFactor = -15f;
 
     [SerializeField] GameObject[] lasers;
+    [SerializeField] Camera cam;
 
+    private float aspect;
 
     private float horizontalInput;
     private float verticalInput;
     // Update is called once per frame
+    private void Start()
+    {
+        aspect = cam.aspect;
+    }
+
     void Update()
     {
         ProcessTranslation();
@@ -31,7 +39,7 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // keeps yRange locked to a 16:9 aspect ratio
-        float yRange = xRange * 9f / 16f;
+        float yRange = xRange / aspect;
 
         float xOffset = horizontalInput * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
